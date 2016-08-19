5243fca8317f655a584f274b52c11857a237a236
Chris L <stylesuxx@gmail.com>
2016-08-20 00:52:45 +0200

    Imporved data types, added some documentation, tested up to 14Bit

--- oversample.cpp
+++ oversample.cpp
@@ -1,7 +1,6 @@
-#include "Arduino.h"
 #include "oversample.h"
 
-Oversample::Oversample(int pin, int resolution)
+Oversample::Oversample(byte pin, byte resolution)
 {
   _pin = pin;
 
@@ -10,47 +9,71 @@ Oversample::Oversample(int pin, int resolution)
   setResolution(resolution);
 }
 
-float Oversample::read()
+double Oversample::read()
 {
-   //...
-   return 0;
+  int scaled = readScaled();
+  double averaged = (scaled * 1.0) / (B00000001 << _additionalBits);
+  return averaged;
 }
 
-void Oversample::setResolution(int resolution)
+long Oversample::readScaled()
 {
-  _resolution = resolution;
-  if(_resolution > 16) {
-   _resolution = 16;
+  /* Do all the measurements as quickly as possible. */
+  for(int i = 0; i < _sampleCount; i++) {
+    _samples[i] = analogRead(_pin);
   }
 
-  if(_resolution < 10) {
-   _resolution = 10;
+  /* Sum all measurements. */
+  long total = 0;
+
+  for(int i = 0; i < _sampleCount; i++) {
+    total += _samples[i];
   }
 
-  int additionalBits = _resolution - 10;
-
-  _samplesCount = 1;
-  if(additionalBits > 0) {
-   _samplesCount += pow(4, additionalBits);
-  }
-
-  delete [] _samples;
-  _samples = new int[_samplesCount];
+  /* Scale by right shifting. */
+  long scaled = total >> _additionalBits;
+  return scaled;
 }
 
-int Oversample::getResolution()
+void Oversample::setResolution(byte resolution)
+{
+  _resolution = sanitizeResolution(resolution);
+  _additionalBits = _resolution - 10;
+  _sampleCount = B00000001 << (_additionalBits * 2);
+
+  if(_samples != 0) {
+    delete [] _samples;
+  }
+
+  _samples = new int [_sampleCount];
+}
+
+byte Oversample::getResolution()
 {
   return _resolution;
 }
 
-void Oversample::setPrescaler(int prescaler)
+void Oversample::setPrescaler(byte prescaler)
 {
-  int mask = B11111000;
+  byte mask = B11111000;
   ADCSRA &= mask;
   ADCSRA |= prescaler;
 }
 
-int Oversample::getPrescaler()
+byte Oversample::getPrescaler()
 {
   return _prescaler;
+}
+
+byte Oversample::sanitizeResolution(byte resolution)
+{
+  if(resolution > 16) {
+    resolution = 16;
+  }
+
+  else if(resolution < 10) {
+    resolution = 10;
+  }
+
+  return resolution;
 }
--- oversample.h
+++ oversample.h
@@ -1,3 +1,5 @@
+#include "Arduino.h"
+
 #ifndef Oversample_h
 #define Oversample_h
 
@@ -8,14 +10,16 @@ class Oversample
      * This may range from 10(no oversampling) to 16(max oversampling)
      * The more resolution you want, the longer the measurement will take.
      */
-    Oversample(int pin, int resolution);
+    Oversample(byte pin, byte resolution);
 
-    /* Returns a measurement with the requested resolution.
-     */
-    float read();
+    /* Returns a measurement with the requested resolution. */
+    double read();
 
-    void setResolution(int resolution);
-    int getResolution();
+    /* Returns the scaled sum of the measurements. */
+    long readScaled();
+
+    void setResolution(byte resolution);
+    byte getResolution();
 
     /* Prescaler
      * 7: 128 (Arduino default)
@@ -27,15 +31,19 @@ class Oversample
      * 1: 2
      * 0: 2
      */
-    void setPrescaler(int prescaler);
-    int getPrescaler();
+    void setPrescaler(byte prescaler);
+    byte getPrescaler();
 
   private:
-    int _pin;
-    int _prescaler;
-    int _resolution;
-    int _samplesCount;
-    int * _samples;
+    byte _pin;
+    byte _prescaler = 4;
+    byte _resolution = 10;
+    byte _baseResolution = 10;
+    byte _additionalBits;
+    int _sampleCount;
+    int * _samples = 0;
+
+    byte sanitizeResolution(byte resolution);
 };
 
 #endif
