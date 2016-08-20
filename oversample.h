690630c053d21b931932ad7940b725058e801490
Chris L <stylesuxx@gmail.com>
2016-08-20 15:22:51 +0200

    Improved documentation

--- oversample.h
+++ oversample.h
@@ -6,22 +6,43 @@
 class Oversample
 {
   public:
-    /* Total Resolution, including oversampled Bit
+    /**
+     * Total Resolution, including oversampled Bits.
+     *
      * This may range from 10(no oversampling) to 16(max oversampling)
      * The more resolution you want, the longer the measurement will take.
+     *
+     * @param pin Analog pin to use.
+     * @param resolution Chosen resolution.
      */
     Oversample(byte pin, byte resolution);
 
-    /* Returns a measurement with the requested resolution. */
+    /**
+     * Run a measurement with the set resolution.
+     *
+     * @return The oversampled, decimated measurement.
+     */
     double read();
 
-    /* Returns the scaled sum of the measurements. */
+    /**
+     * Run a measurement with the set resolution.
+     *
+     * @return The oversampled, non decimated measurement.
+     */
     unsigned long readScaled();
 
+    /**
+     * Set measurment resolution.
+     *
+     * @param resolution Resolution might be between _baseResolution and
+     *                   _maxResolution.
+     */
     void setResolution(byte resolution);
     byte getResolution();
 
-    /* Prescaler
+    /**
+     * Set the ADC's Prescaler.
+     *
      * 7: 128 (Arduino default)
      * 6: 64
      * 5: 32
@@ -30,6 +51,8 @@ class Oversample
      * 2: 4
      * 1: 2
      * 0: 2
+     *
+     * @param prescaler The prescaler to set for the ADC.
      */
     void setPrescaler(byte prescaler);
     byte getPrescaler();
@@ -39,6 +62,7 @@ class Oversample
     byte _prescaler = 4;
     byte _resolution = 10;
     byte _baseResolution = 10;
+    byte _maxResolution = 16;
     byte _additionalBits = 0;
     int _sampleCount = 0;
     int * _samples = 0;
